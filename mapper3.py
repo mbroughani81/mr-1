b6fe1ed1f554f33dced478f6ccf90093761652e5
Mohammad Broughani <gitmbroughani81@gmail.com>
2023-02-26 19:46:00 +0330

    mm

--- mapper3.py
+++ mapper3.py
@@ -34,9 +34,29 @@ line = file.readline()
 res = ""
 
 while line:
+    # The Idea of solution:
+    # for every node v in graph, and each pair of it's neighbors such as
+    # x and y, we suggest y to x once. We will create key value pairs in mapper to show
+    # this suggestions. 
+    # for example, if list of neighbors are as follows:
+    # 1 : 2 3 4
+    # 2 : 1 3
+    # 3 : 1
+    # 4 : 1
+    # than suggestions created from list of node 1's list of neighbors are as follows:
+    # 3 is suggested to 2, 2 is suggested to 3, 4 is suggested to 2, 2 is suggested to 4, 
+    # 3 is suggested to 4, 4 is suggested to 3, .
+    # suggestions created from node 2's list of neighbors are as follows:
+    # 1 is suggested to 3, 3 is suggested to 1
+    # and there is not suggestions created from node 3's and 4's list of neighbors.
+    # Then in next step, in the reducers, we will count the number of 
+    # suggestion for each node, and also filter out the neighbors to don't print
+    # nodes that are neighbor in "might" or "probably" list.
+
+
+    # Code description:
     # From every node v, that has a list of neighbors that are in "line" variable,
     # we will create two groups of key-value pairs.
-
     # first group of key-value pairs:
     # arr is array of (x, y), such x and y are both connected to v. This pair shows that
     # y is suggested to x.
