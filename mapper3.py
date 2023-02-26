3e1947fdc1c60d6dace861d67aed67654519777f
Mohammad Broughani <gitmbroughani81@gmail.com>
2023-02-26 19:32:26 +0330

    mm

--- mapper3.py
+++ mapper3.py
@@ -34,20 +34,20 @@ line = file.readline()
 res = ""
 
 while line:
-    # from every node v, that has a list of neighbors that are in "line" variable,
+    # From every node v, that has a list of neighbors that are in "line" variable,
     # we will create two groups of key-value pairs.
 
     # first group of key-value pairs:
-    # arr is array of (x, y), such x and y are both connected to v. this shows that
+    # arr is array of (x, y), such x and y are both connected to v. This pair shows that
     # y is suggested to x.
-    # if in the end, b is suggested to a at least 4 times, 
-    # y is in probably list of x. if b is suggested 2 or 3 times, b is in might list.
-    # in mapper output, we output the key-value pair (x, "s," + y), to show suggesting y to x
+    # If in the end, node b is suggested to node a at least 4 times, 
+    # b is in probably list of a. if b is suggested 2 or 3 times, b is in might list of a.
+    # In mapper output, we output the key-value pair (x, "s," + y), to show suggesting y to x
     arr = genKeyValuePairs(line)
     for (key, value) in arr:
         print(key + "\t" + "s," + value) # emitting pair used for suggestion
     
-    # we also need to know list of neighbors for each node. so for each pairs of node
+    # We also need to know list of neighbors for each node. So for each pairs of node
     # (x, y), we output the (x, "n," + y). In the reducer processing the key x , we will use this information
     # to don't suggest neighbors that are neighbor to x.
     arr = genNeighborPairs(line)
